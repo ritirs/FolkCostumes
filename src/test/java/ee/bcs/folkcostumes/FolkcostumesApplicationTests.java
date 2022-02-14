42e84e6ff85b3907e3206c3e8fb7353a7f2c3fb5
Riti Rand-Salong <riti.randsalong@gmail.com>
2022-02-14 14:35:46 +0200

    Initial commit

---
+++ src/test/java/ee/bcs/folkcostumes/FolkcostumesApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.folkcostumes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkcostumesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

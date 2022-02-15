072872eca02daec261f6383b8f23cb364d07ef65
Aronza <a.m.voites@gmail.com>
2022-02-15 13:39:00 +0200

    Andmebaasi import failid uuesti. costume_data.sql

--- db/transactions_data.sql
+++ db/transactions_data.sql
@@ -7,3 +7,5 @@ INSERT INTO public.location (id, location) VALUES (4, 'Paranduses');
 INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (1, 1, 1, 1, 20);
 INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (2, 3, 2, 2, 1);
 INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (3, 2, 4, 1, 30);
+
+INSERT INTO public.location_log (id, element_id, location_id, user_id, quantity, balance, transaction_time) VALUES (DEFAULT, 1, 1, 4, 10, 5, '2022-02-16');

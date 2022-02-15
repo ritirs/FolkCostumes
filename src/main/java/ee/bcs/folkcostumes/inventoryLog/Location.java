b7c0461e75f25ca9234d647097ab0dc6adb37100
Aronza <a.m.voites@gmail.com>
2022-02-15 15:10:31 +0200

    Entity lisamine kõikidele tabelitele

--- src/main/java/ee/bcs/folkcostumes/inventoryLog/Location.java
+++ src/main/java/ee/bcs/folkcostumes/inventoryLog/Location.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "location")
+
 public class Location {
 
     @Id
--- src/main/java/ee/bcs/folkcostumes/user/Group.java
+++ src/main/java/ee/bcs/folkcostumes/user/Group.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "group")
+
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

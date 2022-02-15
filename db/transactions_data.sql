8cde20b99f9307c84d7961402c241c79afad975e
Riti Rand-Salong <riti.randsalong@gmail.com>
2022-02-15 11:42:37 +0200

    updated location and location balance data file

--- db/transactions_data.sql
+++ db/transactions_data.sql
@@ -1,13 +1,9 @@
-INSERT INTO public.status_type (id, location) VALUES (1, 'Laos Kultuurikeskuses');
-INSERT INTO public.status_type (id, location) VALUES (2, 'Laos Jüri Gümnaasiumis');
-INSERT INTO public.status_type (id, location) VALUES (3, 'Tantsija käes');
-INSERT INTO public.status_type (id, location) VALUES (4, 'Paranduses');
-
-INSERT INTO public.status (id, status_type_id, user_id) VALUES (DEFAULT, 1, 11);
-INSERT INTO public.status (id, status_type_id, user_id) VALUES (DEFAULT, 2, 10);
-INSERT INTO public.status (id, status_type_id, user_id) VALUES (DEFAULT, 3, 1);
-INSERT INTO public.status (id, status_type_id, user_id) VALUES (DEFAULT, 3, 5);
-INSERT INTO public.status (id, status_type_id, user_id) VALUES (DEFAULT, 4, 10);
+INSERT INTO public.location (id, location) VALUES (1, 'Laos Kultuurikeskuses');
+INSERT INTO public.location (id, location) VALUES (2, 'Laos Jüri Gümnaasiumis');
+INSERT INTO public.location (id, location) VALUES (3, 'Tantsija käes');
+INSERT INTO public.location (id, location) VALUES (4, 'Paranduses');
 
 
-
+INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (1, 1, 1, 1, 20);
+INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (2, 3, 2, 2, 1);
+INSERT INTO public.location_balance (id, location_id, element_id, user_id, quantity) VALUES (3, 2, 4, 1, 30);

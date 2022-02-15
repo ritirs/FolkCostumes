2251ebd500668213ba775ac63007033514188191
Aronza <a.m.voites@gmail.com>
2022-02-15 15:34:05 +0200

    Test uuendamine4 - build gradle ja application.properties

--- src/main/java/ee/bcs/folkcostumes/costume/CostumeRepository.java
+++
@@ -1,6 +0,0 @@
-package ee.bcs.folkcostumes.costume;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CostumeRepository extends JpaRepository<Costume, Integer> {
-}
